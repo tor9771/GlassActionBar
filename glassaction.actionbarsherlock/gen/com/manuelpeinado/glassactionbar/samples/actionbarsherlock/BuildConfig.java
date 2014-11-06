1abdc19d47dbf52aa15229c13775e94b2bac8082
surachet prasertsopa <SURACHET@surachets-MacBook-Pro-2.local>
2014-11-06 16:50:10 +0700

    first commit

---
+++ glassaction.actionbarsherlock/gen/com/manuelpeinado/glassactionbar/samples/actionbarsherlock/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.manuelpeinado.glassactionbar.samples.actionbarsherlock;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
